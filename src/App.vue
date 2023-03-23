6ce20c25bc42d0219c94a8d97d25c2b83d33ba14
Anna <anna@akufen.ca>
2023-03-23 18:06:47 -0400

    init file with paypal working

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Buy a plant!</h1>
+    <Checkout-Item />
+    <Checkout-Payment />
+  </div>
+</template>
+
+<script>
+import CheckoutPayment from './components/Checkout.vue';
+import CheckoutItem from './components/Item.vue';
+export default {
+  name: 'App',
+  components: {
+    CheckoutPayment,
+    CheckoutItem,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  width: 350px;
+  margin: auto;
+  padding: 20px;
+  position: relative;
+  background: #ffffff;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+h1 {
+  font-size: 62px;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/Checkout.vue
@@ -0,0 +1,82 @@
+<template>
+  <div v-if="!paid" id="paypal-button-container"></div>
+  <div v-else id="confirmation">Order complete!</div>
+</template>
+
+<script>
+import { loadScript } from '@paypal/paypal-js';
+
+export default {
+  name: 'Checkout-Payment',
+  beforeCreate: function() {
+    loadScript({ 'client-id': CLIENT_ID, 'intent': 'authorize' }).then((paypal) => {
+      paypal
+        .Buttons({
+          createOrder: this.createOrder,
+          onApprove: this.onApprove,
+        })
+        .render('#paypal-button-container');
+    });
+  },
+  data() {
+    return {
+      paid: false,
+    };
+  },
+  props: {
+    cartTotal: {
+      type: Number,
+      default: 0.01,
+    },
+  },
+  methods: {
+    createOrder: function(data, actions) {
+      console.log('Creating order...');
+      return actions.order.create({
+        purchase_units: [
+          {
+            amount: {
+              value: this.cartTotal,
+            },
+          },
+        ],
+      });
+    },
+    onApprove: function(data, actions) {
+      console.log(actions)
+      // Authorize the transaction
+      return actions.order.authorize()
+      .then((response) => {
+          console.log(response)
+          this.paid = true
+          // Get the authorization id from your payload
+          //const authorizationID = authorizePayload.authorizationID;
+          // Optional message given to purchaser
+          /*
+          alert(`
+            You have authorized this transaction.
+            Order ID: ${data.orderID}
+            Authorization ID: ${authorizationID}
+          `);
+          */
+
+          // Later you can use your server to validate and capture the transaction
+      });
+         
+    }
+  },
+};
+const CLIENT_ID = 'ASkoIUlM5SvjbQTyg5n2g5ODblSom8Te9QIBCVaMWjTk1IaqJDpEH36zdFFuy5mNCYEfieYV8xg1kHxT';
+</script>
+
+<style>
+#paypal-button-container {
+  margin: 30px 0;
+}
+
+#confirmation {
+  color: green;
+  margin-top: 1em;
+  font-size: 2em;
+}
+</style>
