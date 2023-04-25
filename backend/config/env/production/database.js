95e7349299a80889108b4a50b863ce7141562be9
Anna <anna@akufen.ca>
2023-04-24 23:54:08 -0400

    add procfile for heroku

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,25 +1,18 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-        },
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false)},
       },
-      options: {
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-      debug: true,
     },
-  }
-};
+    debug: true,
+  },
+});
